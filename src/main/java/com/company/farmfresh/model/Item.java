45cdf3b5c04b86b4f3ab8ab3f3d7a9a96aaa7c65
Naval Kulkarni <navalkulkarni@gmail.com>
2019-01-21 18:57:00 +0530

    fixed error of @table annotation

--- src/main/java/com/company/farmfresh/model/Item.java
+++ src/main/java/com/company/farmfresh/model/Item.java
@@ -1,15 +1,12 @@
 package com.company.farmfresh.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-@Entity
-@Table(name ="Items")
+@Entity(name="Items")
 public class Item {
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @NotNull
