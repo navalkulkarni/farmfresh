ea3aa769da1b57ac0efbea513df46e5bdc226db0
Naval Kulkarni <navalkulkarni@gmail.com>
2019-01-21 19:26:46 +0530

    fixed @Service annotation

--- src/main/java/com/company/farmfresh/service/ItemServiceImpl.java
+++ src/main/java/com/company/farmfresh/service/ItemServiceImpl.java
@@ -3,9 +3,11 @@ package com.company.farmfresh.service;
 import com.company.farmfresh.dao.ItemDao;
 import com.company.farmfresh.model.Item;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ItemServiceImpl implements ItemService{
 
     @Autowired
