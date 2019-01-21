03b174083094c3c729d1f0175597ec7b5b5c40cb
VeenaMalali13 <46866103+VeenaMalali13@users.noreply.github.com>
2019-01-21 20:48:38 +0530

    Added UserController
    1.SignUp

--- src/main/java/com/company/farmfresh/controllers/UserController.java
+++ src/main/java/com/company/farmfresh/controllers/UserController.java
@@ -1,4 +1,37 @@
 package com.company.farmfresh.controllers;
 
+import com.company.farmfresh.model.User;
+import com.company.farmfresh.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/signup")
+    public String newUser(Map map)
+    {
+        User u=new User();
+        map.put("user",u);
+        return "signup";
+    }
+
+
+    @RequestMapping(value = "/adduser",method = RequestMethod.POST)
+    public String addUser(User u,RedirectAttributes redirectAttributes)
+    {
+        userService.addUser(u);
+        redirectAttributes.addFlashAttribute("success","Signup Successful");
+        return "redirect:/";
+    }
+
+
 }
