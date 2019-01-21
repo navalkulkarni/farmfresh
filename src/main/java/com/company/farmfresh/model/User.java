48d7fa02b75cc1488ba7884cccea30d743480930
VeenaMalali13 <46866103+VeenaMalali13@users.noreply.github.com>
2019-01-21 15:53:58 +0530

    Added user pojo

---
+++ src/main/java/com/company/farmfresh/model/User.java
@@ -0,0 +1,77 @@
+package com.company.farmfresh.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Users")
+public class User {
+    private String name;
+    private String email;
+    private String password;
+    private String mobileNumber;
+    private String address;
+
+    public User() {
+
+    }
+
+    public User(String name, String email, String password, String mobileNumber, String address) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.mobileNumber = mobileNumber;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", mobileNumber='" + mobileNumber + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
